f568aa84016804cbf30941df34ca2311d7687f8e
scottedwardpratt <prattsc@msu.edu>
2022-07-13 08:51:00 -0400

    ...

--- mutcalc/runner.sh
+++ mutcalc/runner.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 \rm -f mutcalc_results/*.txt
-\rm -f crap.log
-nevents=3
+nevents=1000
 for((i=0;i<4;i++))
 do 
 	i0=`expr ${i} \* ${nevents}`
 	if=`expr ${i} \* ${nevents} + ${nevents}`
-	echo i=${i}, i0=${i0}, if=${if}
 	bf_fromhydro ${i} ${i0} ${if}
-	\cp -f mutcalc_results/*.txt mutcalc_input/
+	\cp -f mutcalc_results/*.txt mutcalc_input/ 
 done
